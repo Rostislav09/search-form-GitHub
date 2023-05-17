a1f143cf91209dc6bd92c00560d50f1db744d04b
Rostyslav <109174308+Rostislav09@users.noreply.github.com>
2023-05-17 19:39:06 +0200

    change component UserDetails

--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UserDetails = ({ user }) => {
-  
+  const [followersCount, setFollowersCount] = useState(0);
+  const [followingCount, setFollowingCount] = useState(0);
+
+  useEffect(() => {
+    if (user) {
+      // Выполняем запрос к API для получения числа подписчиков
+      axios.get(user.followers_url)
+        .then(response => setFollowersCount(response.data.length))
+        .catch(error => console.log(error));
+
+      // Выполняем запрос к API для получения числа подписок
+      axios.get(user.following_url)
+        .then(response => setFollowingCount(response.data.length))
+        .catch(error => console.log(error));
+    }
+  }, [user]);
+
   if (!user) {
     return <div className="user-details bg-light border rounded p-3 text-center">No user selected.</div>;
   }
@@ -9,8 +26,6 @@ const UserDetails = ({ user }) => {
   const {
     login,
     avatar_url,
-    followers_url,
-    following_url,
     company,
     email,
     blog
@@ -21,8 +36,8 @@ const UserDetails = ({ user }) => {
       <img src={avatar_url} alt={login} />
       <h1>{login}</h1>
       <ul className="list-unstyled">
-        <li>Followers: {followers_url}</li>
-        <li>Following: {following_url}</li>
+        <li>Followers: {followersCount}</li>
+        <li>Following: {followingCount}</li>
         <li>Company: {company ?? 'No company'}</li>
         <li>Email: {email ?? 'No email'}</li>
         <li>Blog: {blog ?? 'No blog'}</li>
